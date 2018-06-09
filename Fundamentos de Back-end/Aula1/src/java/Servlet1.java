30216cb230ec0fd12972ac3a77c20561fe67ced2
richielybmp <richielybmp@hotmail.com>
2018-06-09 15:22:24 -0300

    Exercicios da aula 1 sobre servlets

---
+++ Fundamentos de Back-end/Aula1/src/java/Servlet1.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Alunoinf_2
+ */
+@WebServlet(name= "Servlet1", urlPatterns = {"/servidor1"})
+public class Servlet1 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession sessao = request.getSession(true);
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Servelet1</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Aula 1, 09-06-2018 <br> Nome: " + request.getParameter("nome") + ""
+                    + "<br> Senha: " + request.getParameter("senha") + ""
+                    + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+            // colocar senha negativa (-1)
+            if (request.getParameter("nome").equals("FBE") && request.getParameter("senha").equals("1234")) {
+                sessao.setAttribute("autenticado", true);
+                
+            } else {
+                sessao.setAttribute("autenticado", false);
+            }
+            if (Integer.parseInt(request.getParameter("senha")) < 0) {
+                response.sendRedirect("error.html");
+            }
+            response.sendRedirect("servlet2");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Fundamentos de Back-end/ExercicioServlet/src/java/ServidorTeste.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+/**
+ *
+ * @author Alunoinf_2
+ */
+public class ServidorTeste extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        HttpSession sessao = request.getSession(true);
+
+        String metodo = request.getMethod();
+        Boolean existeParams = false;
+        if (metodo.equals("GET") || metodo.equals("POST")) {
+            sessao.setAttribute("nome", request.getParameter("nome"));
+            sessao.setAttribute("senha", request.getParameter("senha"));
+        }
+        else if (metodo.equals("DELETE")) {
+            sessao.removeAttribute("nome");
+            sessao.removeAttribute("senha");
+        }
+        else if (metodo.equals("PUT")) {
+            // tratar caso os atributos da sessão estão null
+            if ((sessao.getAttribute("nome")!= null && !((String)sessao.getAttribute("nome")).isEmpty()) ||
+                 sessao.getAttribute("senha")!= null && !((String)sessao.getAttribute("senha")).isEmpty()) {
+                existeParams = true;
+            }
+        }
+
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServidorTeste</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servidor TESTE " + request.getContextPath() + "</h1>");
+            
+            if (existeParams) {
+                out.println("<br>");
+                out.println("<h1> Nome: " + sessao.getAttribute("nome") + "</h1>");
+                out.println("<br>");
+                out.println("<h1> Senha: " + sessao.getAttribute("senha") + "</h1>");
+                out.println("<br>");
+            }
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    /*
+    * Handles the HTTP <code>PUT</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    /*
+    * Handles the HTTP <code>DELETE</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
